c58696bd0265b3672f30abe865683ad10ed59ce4
Dave McKinstry <dmckinstry@users.noreply.github.com>
2025-04-19 21:30:58 +0000

    Restore --seed-all

--- .github/scripts/prepare-lab-env.sh
+++ .github/scripts/prepare-lab-env.sh
@@ -7,24 +7,24 @@ if [[ "$1" == "--delete" || "$1" == "--seed-all" ]]; then
   gh issue list --limit 1000 | awk '{print $1}' | xargs -I {} gh issue delete {} --yes
 fi
 
-# # If "--seed" is passed as an argument, automatically create issues that are meant to be manually created
-# if [[ "$1" == "--seed-all" ]]; then
-#   gh issue create \
-#     --title "Investigate Issue Basics" \
-#     --body "
-# # Issue Basics
+# If "--seed" is passed as an argument, automatically create issues that are meant to be manually created
+if [[ "$1" == "--seed-all" ]]; then
+  gh issue create \
+    --title "Investigate Issue Basics" \
+    --body "
+# Issue Basics
 
-# This task describes the lab steps required to complete the Issue Basics tasks.  As a template item, this description was filled out for you in advance (to save typing).  Generally, you will use templates to get started with a standard format and content but add additional information.  In this case, the issue is complete as-is...
+This task describes the lab steps required to complete the Issue Basics tasks.  As a template item, this description was filled out for you in advance (to save typing).  Generally, you will use templates to get started with a standard format and content but add additional information.  In this case, the issue is complete as-is...
 
-# To complete this task you will:
-# - [x] Use an issue template (done)
-# - [x] Review markdown used to format content in GitHub. You're looking at it now, but if you aren't already familiar with markdown you can refer to [this link](https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github).  If you've already saved it and want to review the source markdown you can edit the description to see the unformatted text.
-# - [ ] Apply Labels
-# - [ ] Apply Milestones
-# - [ ] Query Issues
-# - [ ] Verify automation triggered by issues" \
-#     --assignee "@me" 
-# fi
+To complete this task you will:
+- [x] Use an issue template (done)
+- [x] Review markdown used to format content in GitHub. You're looking at it now, but if you aren't already familiar with markdown you can refer to [this link](https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github).  If you've already saved it and want to review the source markdown you can edit the description to see the unformatted text.
+- [ ] Apply Labels
+- [ ] Apply Milestones
+- [ ] Query Issues
+- [ ] Verify automation triggered by issues" \
+    --assignee "@me" 
+fi
 
 # Function to load issue seeds from a JSON file
 load_issue_seeds() {
